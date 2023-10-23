7c40a813ee06f5cd02d14efc95689c5c28e2d6d3
Juan <125908889+JuanPanizzi@users.noreply.github.com>
2023-10-23 15:26:35 -0300

    "new proyect added"

--- src/recursos/config.js
+++ src/recursos/config.js
@@ -3,42 +3,52 @@ import feriados from '../recursos/feriados.GIF'
 import memoTest from '../recursos/memo-test.png'
 import juego from '../recursos/juego.png'
 import buscadorPeliculas from '../recursos/buscador-peliculas.png'
+import superAwp from '../recursos/awp-capture.png'
 
 
-export const infoCard = [{
-    titulo: 'Caracteres por Minuto',
-    imagen: hp,
-    descripcion: 'App para practicar habilidades de Tipeo con temática de Harry Potter. Realizado con React y Bootstrap.',
-    linkGithub: "https://github.com/JuanPanizzi/Ejercitador-de-tipeo-app.git",
-    linkDemo: "https://ejercitador-de-tipeo-app.vercel.app/"
-}, 
-{
-    titulo: 'App de Feriados',
-    imagen: feriados,
-    descripcion: 'App para ver los próximos feriados de Argentina, y también los que pasaron. Realizado con React, Sass y Css.',
-    linkGithub: 'https://github.com/JuanPanizzi/app-feriados.git',
-    linkDemo: 'https://zippy-basbousa-85e837.netlify.app/'
-},
-{
-    titulo: 'Memo Test',
-    imagen: memoTest,
-    descripcion: 'Juego para practicar la memoria con temática de dibujitos animados de los años noventa. Realizado con React y Bootstrap.',
-    linkGithub: 'https://github.com/JuanPanizzi/memo-test.git',
-    linkDemo: 'https://memo-test-khaki.vercel.app/'
-},
-{
-    titulo: 'Dev-Game',
-    imagen: juego,
-    descripcion: 'Juego para sortear obstáculos basados en errores de diseño UI y UX y divertirse un ratito. Hecho con Vite, React y Css.',
-    linkGithub: 'https://github.com/JuanPanizzi/dev-game.git',
-    linkDemo: 'https://dev-game.vercel.app/'
-},
-{
-    titulo: 'Buscador de Películas',
-    imagen: buscadorPeliculas,
-    descripcion: 'Aplicación web para buscar tus películas, series y programas favoritos. Realizado con React, Vite y Css.',
-    linkGithub: 'https://github.com/JuanPanizzi/buscador-peliculas.git',
-    linkDemo: 'https://buscador-peliculas-ruby.vercel.app/'
-}
+export const infoCard = [
+
+    {
+        titulo: 'App de Feriados',
+        imagen: feriados,
+        descripcion: 'App para ver los próximos feriados de Argentina, y también los que pasaron. Realizado con React, Sass y Css.',
+        linkGithub: 'https://github.com/JuanPanizzi/app-feriados.git',
+        linkDemo: 'https://zippy-basbousa-85e837.netlify.app/'
+    }, 
+    {
+        titulo: 'AWP para el Super Mercado',
+        imagen: superAwp,
+        descripcion: 'AWP instalable para controlar los productos y gastos en el super. Realizado con Service Workers, React y Css.',
+        linkGithub: 'https://github.com/JuanPanizzi/Control-Gastos-Supermercado.git',
+        linkDemo: 'https://controlador-gastos-supermercado.vercel.app/'
+    }, 
+    {
+        titulo: 'Caracteres por Minuto',
+        imagen: hp,
+        descripcion: 'App para practicar habilidades de Tipeo con temática de Harry Potter. Realizado con React y Bootstrap.',
+        linkGithub: "https://github.com/JuanPanizzi/Ejercitador-de-tipeo-app.git",
+        linkDemo: "https://ejercitador-de-tipeo-app.vercel.app/"
+    },
+    {
+        titulo: 'Memo Test',
+        imagen: memoTest,
+        descripcion: 'Juego para practicar la memoria con temática de dibujitos animados de los años noventa. Realizado con React y Bootstrap.',
+        linkGithub: 'https://github.com/JuanPanizzi/memo-test.git',
+        linkDemo: 'https://memo-test-khaki.vercel.app/'
+    },
+    {
+        titulo: 'Dev-Game',
+        imagen: juego,
+        descripcion: 'Juego para sortear obstáculos basados en errores de diseño UI y UX y divertirse un ratito. Hecho con Vite, React y Css.',
+        linkGithub: 'https://github.com/JuanPanizzi/dev-game.git',
+        linkDemo: 'https://dev-game.vercel.app/'
+    },
+    {
+        titulo: 'Buscador de Películas',
+        imagen: buscadorPeliculas,
+        descripcion: 'Aplicación web para buscar tus películas, series y programas favoritos. Realizado con React, Vite y Css.',
+        linkGithub: 'https://github.com/JuanPanizzi/buscador-peliculas.git',
+        linkDemo: 'https://buscador-peliculas-ruby.vercel.app/'
+    }
 
 ] 
